b8e0e3b2e4127dffb0f7cacbb59c2f867a2c19ae
ahmki <aleksi.heinimaki1@gmail.com>
2022-03-25 12:42:52 +0200

    10.8 no styling

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,43 @@
+import FormikTextInput from './FormikTextInput';
+import { View, Pressable, StyleSheet } from 'react-native';
 import Text from './Text';
+import { Formik } from 'formik';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: theme.colors.primary,
+  },
+});
+
+const initialValues = {
+  username: '',
+  password: '',
+};
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  
+  const onSubmit = (values) => {
+    console.log('values', values);
+  };
+
+  return (
+
+    <View>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        {({ handleSubmit }) => (
+          <>
+            <FormikTextInput name="username" placeholder="Username" />
+            <FormikTextInput name="password" placeholder="Password" secureTextEntry={true}/>
+            <Pressable onPress={handleSubmit}>
+              <Text style={styles.button} color="textWhite">Sign in</Text>
+            </Pressable>
+          </>
+        )}
+
+      </Formik>
+    </View>
+  )
 };
 
 export default SignIn;
